f6c51448f22350ee1154039c4d1c600afef5fbc4
Denis <denis-sergeev-85@inbox.ru>
2023-11-28 13:46:58 +0300

    сделан блок услуг

--- slider.js
+++ slider.js
@@ -1,11 +1,34 @@
 var swiper = new Swiper(".bannerSwiper", {
   navigation: {
-    nextEl: ".button-next",
-    prevEl: ".button-prev",
+    prevEl: ".button-next",
+    nextEl: ".button-prev",
   },
 
   scrollbar: {
     el: ".swiper-scrollbar",
     hide: true,
+  },
+});
+
+var swiper1 = new Swiper(".serviceSlider", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  navigation: {
+    prevEl: ".service-next",
+    nextEl: ".service-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    425: {
+      slidesPerView: 1.5,
+    },
+    850: {
+      slidesPerView: 2.5,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
 });
