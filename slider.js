9df9bd7cde12c0f620fdbc8f0c7e47ba68faa6ab
Denis <denis-sergeev-85@inbox.ru>
2023-11-28 11:07:48 +0300

    сделан блок баннера

---
+++ slider.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".bannerSwiper", {
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
