e852841fb7f20297c0155bcdda54cdd22e141950
Denis <denis-sergeev-85@inbox.ru>
2023-12-09 13:53:27 +0300

    сделана страница карты

---
+++ js/map.js
@@ -0,0 +1,51 @@
+/*
+
+ymaps.ready(init);
+var myMap, myPlacemark;
+
+function init() {
+  myMap = new ymaps.Map("map", {
+    center: [55.78, 49.128],
+    zoom: 15,
+  });
+
+  myPlacemark = new ymaps.Placemark([55.78, 49.128], {
+    hintContent: "!",
+    balloonContent: "1",
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+}
+*/
+
+let center = [55.795464, 49.108049];
+
+function init() {
+  let map = new ymaps.Map("map-test", {
+    center: center,
+    zoom: 17,
+  });
+
+  let placemark = new ymaps.Placemark(
+    center,
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "images/placemark.svg",
+      iconImageSize: [80, 80],
+      iconImageOffset: [0, 0],
+    }
+  );
+
+  map.controls.remove("geolocationControl");
+  map.controls.remove("searchControl");
+  map.controls.remove("trafficControl");
+  map.controls.remove("typeSelector");
+  map.controls.remove("fullscreenControl");
+  map.controls.remove("zoomControl");
+  map.controls.remove("rulerControl");
+
+  map.geoObjects.add(placemark);
+}
+
+ymaps.ready(init);
