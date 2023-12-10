fa255e6ef7bafa97626430e307032cc3572088dc
Denis <denis-sergeev-85@inbox.ru>
2023-12-10 10:49:24 +0300

    исправлены модальные окна

--- js/header.js
+++ js/header.js
@@ -1,34 +1,16 @@
 const openMenu = document.querySelector(".header__open-menu");
 const closeMenu = document.querySelector(".header__close-menu");
-const modal = document.querySelector(".modal");
-
-const openConsultation = document.querySelector(".consultation__open");
+const modalHeader = document.querySelector(".modal");
+const openConsultation = document.querySelectorAll(".consultation__open");
 const modalConsultation = document.querySelector(".modal-consultation");
 const openCity = document.querySelector(".open-modal-city");
 const modalOpen = document.querySelector(".modal-city");
-//const closeConsultation = document.querySelector(".close-modal-cons");
 const closeCity = document.querySelectorAll(".close-modal");
 const popup = document.querySelector(".popup");
 const popup2 = document.querySelector(".popup2");
 const darkening = document.querySelectorAll(".darkening");
 
-function openModal() {
-  openMenu.style.display = "none";
-  closeMenu.style.display = "block";
-  modal.style.display = "block";
-}
-
-function closeModal() {
-  openMenu.style.display = "block";
-  closeMenu.style.display = "none";
-  modal.style.display = "none";
-}
-
-openMenu.addEventListener("click", openModal);
-closeMenu.addEventListener("click", closeModal);
-
 function toggleModal(event) {
-  console.log(event.target);
   if (event.target.matches(".consultation__open")) {
     modalConsultation.style.display = "block";
     document.body.style.overflow = "hidden";
@@ -42,20 +24,27 @@ function toggleModal(event) {
   } else if (event.target.matches(".header__open-menu")) {
     openMenu.style.display = "none";
     closeMenu.style.display = "block";
-    modal.style.display = "block";
+    modalHeader.style.display = "block";
+    document.body.style.overflow = "hidden";
+  } else if (event.target.matches(".header__close-menu")) {
+    openMenu.style.display = "block";
+    closeMenu.style.display = "none";
+    modalHeader.style.display = "none";
+    document.body.style.overflow = "auto";
   } else if (event.target.matches(".darkening")) {
     popup.style.display = "none";
     popup2.style.display = "none";
     closeMenu.style.display = "none";
-    modal.style.display = "none";
+    modalHeader.style.display = "none";
     openMenu.style.display = "block";
     document.body.style.overflow = "auto";
   }
 }
 
+closeMenu.addEventListener("click", toggleModal);
 openCity.addEventListener("click", toggleModal);
-openConsultation.addEventListener("click", toggleModal);
-openMenu.addEventListener("click", toggleModal);
 
+openMenu.addEventListener("click", toggleModal);
 closeCity.forEach((btn) => btn.addEventListener("click", toggleModal));
 darkening.forEach((btn) => btn.addEventListener("click", toggleModal));
+openConsultation.forEach((btn) => btn.addEventListener("click", toggleModal));
