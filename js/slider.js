f14fc5683d32991e8793738ef7067bf9ff678a23
Denis <denis-sergeev-85@inbox.ru>
2023-12-11 10:04:47 +0300

    исправлена картика баннер-слайдера

--- js/slider.js
+++ js/slider.js
@@ -7,18 +7,6 @@ var swiper = new Swiper(".bannerSwiper", {
   scrollbar: {
     el: ".swiper-scrollbar",
     hide: false,
+    draggable: true,
   },
 });
-
-/*
-
-var swiper = new Swiper(".projectsSlider", {
-  loop: true,
-  slidesPerView: 2,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".projects-next",
-    prevEl: ".projects-prev",
-  },
-});
-*/
