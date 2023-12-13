c835d8b316a14da3f0893e85bcd771018a21df22
Denis <denis-sergeev-85@inbox.ru>
2023-12-13 13:52:00 +0300

    исправлена страница о компании

--- js/commandSlider.js
+++ js/commandSlider.js
@@ -1,14 +1,14 @@
 var swiperCommand = new Swiper(".commandSlider", {
   slidesPerView: 4,
   loop: true,
-  spaceBetween: 30,
+  spaceBetween: 20,
   navigation: {
     prevEl: ".command-next",
     nextEl: ".command-prev",
   },
 
   breakpoints: {
-    320: { slidesPerView: 1.5, spaceBetween: 10 },
+    320: { slidesPerView: 1.5, spaceBetween: 15 },
     425: { slidesPerView: 2 },
     1024: { slidesPerView: 3 },
     1920: { slidesPerView: 4 },
