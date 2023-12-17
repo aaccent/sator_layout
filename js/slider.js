d5304a7185ffee288cb1148269b6c77aefed11d4
Denis <denis-sergeev-85@inbox.ru>
2023-12-17 19:17:11 +0300

    исправлена пагинация слайдера баннера

--- js/slider.js
+++ js/slider.js
@@ -11,4 +11,14 @@ var swiper = new Swiper(".bannerSwiper", {
     dragSize: 150,
     snapOnRelease: true,
   },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      let allSlides = document.querySelectorAll(".banner-card").length;
+      let slideFactor = allSlides + 0.4;
+      return `<span class="${className}" style="width: calc(100% / ${slideFactor})">
+      </span>`;
+    },
+  },
 });
