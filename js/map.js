52766089591aeace0d499ed5892d199cbe33a219
sevastyan <foxhable@gmail.com>
2024-02-29 17:28:48 +0300

    feat: placemark to map in contacts

--- js/map.js
+++ js/map.js
@@ -1,31 +1,38 @@
-let center = [55.795464, 49.108049];
+void function() {
+  const mapContainer = document.querySelector('.map')
 
-function init() {
-  let map = new ymaps.Map("map-test", {
-    center: center,
-    zoom: 17,
-  });
+  if (!mapContainer) return
 
-  let placemark = new ymaps.Placemark(
-    center,
-    {},
-    {
-      iconLayout: "default#image",
-      iconImageHref: "images/placemark.svg",
-      iconImageSize: [80, 80],
-      iconImageOffset: [0, 0],
-    }
-  );
+  let center = mapContainer.dataset.coordinates.split(',').map(Number);
 
-  map.controls.remove("geolocationControl");
-  map.controls.remove("searchControl");
-  map.controls.remove("trafficControl");
-  map.controls.remove("typeSelector");
-  map.controls.remove("fullscreenControl");
-  map.controls.remove("zoomControl");
-  map.controls.remove("rulerControl");
+  function init() {
+    let map = new ymaps.Map(mapContainer, {
+      center: center,
+      zoom: 17,
+    });
 
-  map.geoObjects.add(placemark);
-}
+    let placemark = new ymaps.Placemark(
+      center,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "../images/placemark.svg",
+        iconImageSize: [80, 80],
+        iconImageOffset: [0, 0],
+      }
+    );
 
-ymaps.ready(init);
+    map.controls.remove("geolocationControl");
+    map.controls.remove("searchControl");
+    map.controls.remove("trafficControl");
+    map.controls.remove("typeSelector");
+    map.controls.remove("fullscreenControl");
+    map.controls.remove("zoomControl");
+    map.controls.remove("rulerControl");
+
+    map.geoObjects.add(placemark);
+  }
+
+  ymaps.ready(init);
+
+}()
